4be357339bcd2e6300dddad5fb8b84ca10e8b959
Kartik Agarwal <103099845+kartikhustles@users.noreply.github.com>
2023-10-29 15:06:25 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at Vellore Institute of Technology. I enjoy creating unique and simplistic Websites in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
+  "UI/UX Design",
+  "Full Stack Web Developer",
+  "Graphic design",
+  "Inclusive Design",
   "Focus group testing",
   "Mobile user interfaces",
-  "Graphic design",
 ];
 
 /**
