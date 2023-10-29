328aad202ba9feb0375cd0a50feb440be76ae8ed
Kartik Agarwal <103099845+kartikhustles@users.noreply.github.com>
2023-10-29 15:05:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Kartik Agarwal",
+  title: "UI/UX Designer & Web Developer",
+  email: "kartikhustles@gmail.com",
+  gitHub: "kartikhustles",
+  instagram: "hey.its.kartik",
+  linkedIn: "kartikhustles",
+  medium: "kartikhutles",
+  twitter: "kartikhutles",
+  youTube: "kartikhustles",
 };
 
 const primaryColor = "#4E567E";
