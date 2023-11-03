d55a40031c60d0971bbb9643efc0a995bdc89ead
Kartik Agarwal <kartikhustles@gmail.com>
2023-11-03 14:17:53 +0530

    Changes updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,16 +13,17 @@ import PropTypes from "prop-types";
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  *
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/kartik.jpeg";
+import image from "../images/kartik.jpg";
 
-const imageAltText = "Adult Coder in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText =
+  "Adult Coder in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 
 const Home = ({ name, title }) => {
   return (
